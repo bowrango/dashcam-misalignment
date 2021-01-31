f1ade12482f826e334dc3e523fe97e890d31e86c
bowrango <mjb1085@wildcats.unh.edu>
2021-01-30 23:47:34 -0500

    simplified

--- supervised_sol.py
+++ supervised_sol.py
@@ -1,37 +1,49 @@
-
 import numpy as np
 import cv2 as cv
 
 from sklearn.neural_network import MLPRegressor
 
+def encode_features(video_f, watch=False):
+    # put the cropped grey-scale images into a 2D array
+    # each row represents the features for the image
+    cap = cv.VideoCapture(video_f)
+    features = []
+
+    while cap.isOpened():
+        # full color image
+        success, frame = cap.read()
+        if frame is None:
+            break
+        cropped_frame = frame[1:100, 1:100]
+        # cropped grey image
+        cropped_grey = cv.cvtColor(cropped_frame, cv.COLOR_BGR2GRAY)
+        if watch:
+            cv.imshow('grey', frame)
+        k = cv.waitKey(30) & 0xff
+        if k == 27:
+            break
+        features.append(cropped_grey.flatten())
+    return features
+
+
+def read_angles(angles_f):
+    return np.loadtxt(angles_f)
+
+
 # ~~~SUPERVISED LEARNING~~~
 
-# put labeled angle data into a Fx2 array, where F is the number of frames
-y = np.loadtxt(f"labeled/{0}.txt")
+y = read_angles(f"labeled/{0}.txt")
+X = encode_features(f"tests/{0}.avi")
 
-# put cropped grey-scale images into a 2D array
-# each row represents the features for the image
-cap = cv.VideoCapture('tests/0.mp4')
-X = []
+true_y = read_angles(f"labeled/{1}.txt")
+test_X = encode_features(f"tests/{1}.avi")
 
-while cap.isOpened():
-    # full color image
-    success, frame = cap.read()
-    if frame is None:
-        break
-    cropped_frame = frame[1:100, 1:100]
-    # cropped grey image
-    cropped_grey = cv.cvtColor(cropped_frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('grey', frame)
-    k = cv.waitKey(30) & 0xff
-    if k == 27:
-        break
-    # flatten cropped image because MLPClassifier only works with 2D data
-    flat_grey = cropped_grey.flatten()
-    X.append(flat_grey)
-
+# handle for nan occurrences
 # Shape X: (1200, 9801)
 # Shape y: (1200, 2)
 
+# TODO: Pre-cache video frame for debugging and consider resizing images
 clf = MLPRegressor()
 clf.fit(X, y)
+
+print(clf.predict(test_X))

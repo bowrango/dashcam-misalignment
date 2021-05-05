bdca23d2a2490e2531696e886eab3d579b709a88
Matt Bowring <mjb1085@wildcats.unh.edu>
2021-05-04 20:35:39 -0400

    easy bounding box

--- optical_flow_sol.py
+++ optical_flow_sol.py
@@ -4,36 +4,41 @@ import cv2 as cv
 # ~~~LUCAS-KANADE OPTICAL FLOW~~~
 
 # See if there are better video formats, this is super slow to convert online
-cap = cv.VideoCapture('tests/0.mp4')
+cap = cv.VideoCapture('labeled/0.mp4')
 
-#   params for ShiTomasi corner detection
+# params for ShiTomasi corner detection
 feature_params = dict(maxCorners=10,
                       qualityLevel=0.3,
                       minDistance=50,
                       blockSize=7)
 
-# Parameters for lucas kanade optical flow
+# params for lucas kanade optical flow
 lk_params = dict(winSize=(15, 15),
                  maxLevel=2,
                  criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# Create some random colors for tracking
+# create some random colors for tracking
 color = np.random.randint(0, 255, (100, 3))
 
-# Take first frame and find corners in it
+# take first frame and find corners in it
 ret, old_frame = cap.read()
 old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 
-# the mask focuses on a particular region
+# mask focuses on a particular region
 focus_on = np.zeros_like(old_gray)
-focus_on[1:300, 1:500] = 255
+H, W = focus_on.shape
 
-p0 = cv.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+# rectangular region to track features
+tl_corner = (int(np.rint(2*H/3)), int(np.rint(W/5)))
+br_corner = (int(np.rint(H)), int(np.rint(4*W/5)))
+focus_on[tl_corner[0]:br_corner[0], tl_corner[1]:br_corner[1]] = 255
 
-# Create a mask image for drawing purposes
+p0 = cv.goodFeaturesToTrack(old_gray, mask=focus_on, **feature_params)
+
+# Create a mask image for point drawing purposes
 mask = np.zeros_like(old_frame)
 
-# TODO: this fails in a weird error when the tracking points run out of frame
+# TODO: this fails when the tracking points run out of frame
 while 1:
     ret, frame = cap.read()
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -53,6 +58,8 @@ while 1:
         frame = cv.circle(frame, (a, b), 5, color[i].tolist(), -1)
 
     img = cv.add(frame, mask)
+    # draw rect on mask 
+    rect = cv.rectangle(img, (tl_corner[1],tl_corner[0]), (br_corner[1],br_corner[0]), (255, 0, 0), 2)
     cv.imshow('frame', img)
     k = cv.waitKey(30) & 0xff
     if k == 27:

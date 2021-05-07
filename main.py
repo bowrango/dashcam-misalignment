55e7d486f915263a8967102fbcd561b603e3d74c
Matt Bowring <mjb1085@wildcats.unh.edu>
2021-05-06 23:37:45 -0400

    functioning rnn

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import numpy as np
 import cv2 as cv
+import torch 
+from torch import nn
+import numpy as np
+
+import pickle
 import time
+import matplotlib.pyplot as plt
+
 
 def auto_canny(image, sigma=0.33, watch=False):
+
+    # Otsu's method for thresholding
+    high_thresh, image = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    low_thresh = 0.5*high_thresh
+    otsu_edge = cv.Canny(image, low_thresh, high_thresh)
+    if watch:
+        cv.imshow('otsu-canny', otsu_edge)
+
     # compute the median of the single channel pixel intensities
     v = np.median(image)
     # apply automatic Canny edge detection using the computed median
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
-    edged = cv.Canny(image, lower, upper)
+    auto_edge = cv.Canny(image, lower, upper)
     if watch:
-        cv.imshow('autocanny', edged)
-    return edged
+        cv.imshow('auto-canny', auto_edge)
+    return otsu_edge
 
 def read_angles(angles_f, ffill=0):
 
@@ -20,7 +34,7 @@ def read_angles(angles_f, ffill=0):
     #   - replace these occurances with previous real angle value
     #   - assumption is the angle doesnt change much during low speeds 
 
-    arr = np.loadtxt(angles_f, dtype='float16')
+    arr = np.loadtxt(angles_f, dtype=np.float32)
     mask = np.isnan(arr)
     tmp = arr[0].copy()
     arr[0][mask[0]] = ffill
@@ -55,17 +69,17 @@ def extract_features(video_f, watch=False):
         success, frame = cap.read()
         if success:
             # === Canny Edge Detection for Tracking the Dash Horizon ==
-            kernel = (1,5) # TODO: experiment with vert/horiz kernels
-            frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            canny_img = frame_gray[tl_corner[0]:br_corner[0], tl_corner[1]:br_corner[1]]
-            canny_img = cv.GaussianBlur(canny_img, kernel, 0)
+            kernel = (3,3) # TODO: experiment with vert/horiz kernels
+            canny_img = frame[tl_corner[0]:br_corner[0], tl_corner[1]:br_corner[1]]
+            canny_gray = cv.cvtColor(canny_img, cv.COLOR_BGR2GRAY)
+            canny_blur = cv.GaussianBlur(canny_gray, kernel, 0)
 
-            edges = auto_canny(canny_img, watch=watch)
+            edges = auto_canny(canny_blur, watch=watch)
             features.append(edges.flatten())
 
             # draw rect on video 
             if watch:
-                rect = cv.rectangle(frame, (tl_corner[1],tl_corner[0]), (br_corner[1],br_corner[0]), (255, 0, 0), 2)
+                #rect = cv.rectangle(frame, (tl_corner[1],tl_corner[0]), (br_corner[1],br_corner[0]), (255, 0, 0), 2)
                 cv.imshow(f"{video_f}", frame)
             k = cv.waitKey(30) & 0xff
 
@@ -77,17 +91,66 @@ def extract_features(video_f, watch=False):
     
     end = time.time()
     print(f"Processing Time: {round(end-start,2)} (s)")
-    return np.asarray(features)
+    return np.asarray(features, dtype=np.float32) # float32 for converting torch FloatTensor
 
 # 1. Extract features from training videos using Canny edge detection
 # 2. Read in labeled angles
 # 3. Train RNN
 
 if __name__ == "__main__":
+    from rnn import RNN
 
     # TODO: should be able to handle .avi files, use .mp4 for now 
-    X = extract_features("labeled/0.mp4", watch=True)
-    y = read_angles("labeled/0.txt")
+    # X = extract_features("labeled/0.mp4", watch=True)
+    # np.save('X', X)
+
+    X = np.load('X.npy')
+    Y = read_angles("labeled/0.txt")
+
+    pitch_gt = Y[:,0]
+    yaw_gt = Y[:,1]
+
+    torch.manual_seed(1)    # reproducible
+
+    rnn = RNN()
+    optimizer = torch.optim.Adam(rnn.parameters(), lr=0.02)   # optimize all cnn parameters
+    loss_func = nn.MSELoss()
+    h_state = None      # for initial hidden state
+
+    pitch = np.zeros(len(X))
+    yaw = np.zeros(len(X))
+
+    # sequential training one frame at a time
+    for step in range(len(X)):
+        
+        x = X[step].reshape(1, 1, 28618) # shape (batch, time_step, input_size)
+        x_t = torch.from_numpy(x)
+
+        y = Y[step].reshape(1, 1, 2)
+        y_t = torch.from_numpy(y)
+
+        # rnn must take tensors as input  
+        prediction, h_state = rnn(x_t, h_state)   # rnn output
+        pitch[step] = prediction.data.numpy().flatten()[0]
+        yaw[step] = prediction.data.numpy().flatten()[1]
     
-    print(X.shape)
-    print(y.shape)
+        # !! next step is important !!
+        h_state = h_state.data        # repack the hidden state, break the connection from last iteration
+
+        loss = loss_func(prediction, y_t)       # calculate loss
+        optimizer.zero_grad()                   # clear gradients for this training step
+        loss.backward()                         # backpropagation, compute gradients
+        optimizer.step()                        # apply gradients
+
+        print(f"MSE Loss: {loss}")
+        
+    plt.figure(1, figsize=(12, 5))
+    plt.plot(np.array(range(len(X))), pitch, 'r')
+    plt.plot(np.array(range(len(X))), pitch_gt, 'b')
+    plt.show()
+
+    plt.figure(2, figsize=(12, 5))
+    plt.plot(np.array(range(len(X))), yaw, 'r')
+    plt.plot(np.array(range(len(X))), yaw_gt, 'b')
+    plt.show()
+
---
+++ rnn.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+# torch.manual_seed(1)    # reproducible
+
+# hyper Parameters
+TIME_STEP = 1      # rnn time step
+INPUT_SIZE = 28618      # rnn input size
+LR = 0.02           # learning rate
+
+# show data
+# steps = np.linspace(0, np.pi*2, 100, dtype=np.float32)  
+# x_np = np.sin(steps)
+# y_np = np.cos(steps)
+# plt.plot(steps, y_np, 'r-', label='target (cos)')
+# plt.plot(steps, x_np, 'b-', label='input (sin)')
+# plt.legend(loc='best')
+# plt.show()
+
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+
+        self.rnn = nn.RNN(
+            input_size=INPUT_SIZE,
+            hidden_size=32,     # rnn hidden unit
+            num_layers=1,       # number of rnn layer
+            batch_first=True,   # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+        #self.out = nn.Linear(28618, 2)
+        self.out = nn.Linear(32, 2)
+
+    def forward(self, x, h_state):
+        # x (batch, time_step, input_size)
+        # h_state (n_layers, batch, hidden_size)
+        # r_out (batch, time_step, hidden_size)
+        r_out, h_state = self.rnn(x, h_state)
+
+        outs = []    # save all predictions
+        for time_step in range(r_out.size(1)):    # calculate output for each time step
+            #outs.append(self.out(r_out[:, time_step, :]))
+            outs.append(self.out(r_out[:, time_step, :]))
+        return torch.stack(outs, dim=1), h_state
+
+        # instead, for simplicity, you can replace above codes by follows
+        # r_out = r_out.view(-1, 32)
+        # outs = self.out(r_out)
+        # outs = outs.view(-1, TIME_STEP, 1)
+        # return outs, h_state
+        
+        # or even simpler, since nn.Linear can accept inputs of any dimension 
+        # and returns outputs with same dimension except for the last
+        # outs = self.out(r_out)
+        # return outs
